95114cc5f963a819f8cff56f26c3fa9e1d29d5bb
michcode <mjszkf@icloud.com>
2022-03-27 12:02:02 +0100

    Add files via upload

--- rowanBirthday.py
+++ rowanBirthday.py
@@ -1,7 +1,19 @@
-message = "hello world"
-ratio = 1/(300/200)
+import os
+os.system('')
+message = "non binary flag moment"
+ratio = 1/((300/200)*2)
 lines = int(len(message)*ratio)
-for y in range(lines):
-    print("")
-    for x in range(len(message)):
-        print(print('\x1b[6;30;42m' + 'h' + '\x1b[0m'), end = "")
+if lines < 4:
+    lines = 4
+while lines%4!=0:
+    lines = lines -1
+
+colourHeight = int(lines/4)
+colours = ['\33[103m', '\33[107m', '\33[45;97m', '\33[40;97m']
+
+for colour in colours:
+    for y in range(colourHeight):
+        print("")
+        for x in range(len(message)):
+            print(colour + message[x] + '\33[0m', end = "")
+print()
